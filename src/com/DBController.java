0f30f08c938f6ec88600cff94128bc03692d1bf3
RuselKim <sus.metalist@gmail.com>
2017-10-15 02:26:34 +0300

    Updated CRUD in contoller

--- src/com/DBController.java
+++ src/com/DBController.java
@@ -12,7 +12,7 @@ public class DBController {
 		Statement selectCars;
 		try {
 			selectCars = connection.createStatement();
-			String query = "Select * from cars";
+			String query = "Select * from cars;";
 			ResultSet result;
 			result = selectCars.executeQuery(query);
 	
@@ -35,7 +35,7 @@ public class DBController {
 		Statement selectShops;
 		try {
 			selectShops = connection.createStatement();
-			String query = "Select * from shops";
+			String query = "Select * from shops;";
 			ResultSet result;
 			result = selectShops.executeQuery(query);
 	
@@ -53,7 +53,7 @@ public class DBController {
 	}
 	
 	public void insertCars (Cars car){
-		String query = "Insert into cars (model,price,shops_id)values(?,?,?)";
+		String query = "Insert into cars (model,price,shops_id)values(?,?,?);";
 		Connection connection = con.getConnection();
 		try {
 			PreparedStatement pstmt = connection.prepareStatement(query);
@@ -61,6 +61,7 @@ public class DBController {
 			pstmt.setInt(2,car.getPrice());
 			pstmt.setInt(3,car.getShopID().getId());
 			pstmt.executeUpdate();
+			System.out.println("insert into cars complit");
 		} catch (SQLException e) {
 			System.out.println("bad query");
 		}
@@ -69,13 +70,14 @@ public class DBController {
 	}
 	
 	public void insertShops (Shops shop){
-		String query = "Insert into shops (name,adress)values(?,?)";
+		String query = "Insert into shops (name,adress)values(?,?);";
 		Connection connection = con.getConnection();
 		try {
 			PreparedStatement pstmt = connection.prepareStatement(query);
 			pstmt.setString(1,shop.getName());
 			pstmt.setString(2,shop.getAdress());
 			pstmt.executeUpdate();
+			System.out.println("insert into shops complit");
 		} catch (SQLException e) {
 			System.out.println("bad query");
 		}
@@ -84,19 +86,60 @@ public class DBController {
 	}
 	
 	public void updateCars(Cars car){
-		
+		String query = "Update cars set model=?, price=?, shops_id=? where id=?;";
+		Connection connection = con.getConnection();
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(query);
+			pstmt.setString(1,car.getModel());
+			pstmt.setInt(2,car.getPrice());
+			pstmt.setInt(3,car.getShopID().getId());
+			pstmt.setInt(4,car.getId());
+			pstmt.executeUpdate();
+			System.out.println("cars is updated");
+		} catch (SQLException e) {
+			System.out.println("bad query");
+		}
 	}
 	
 	public void updateShops(Shops shop){
-		
+		String query = "Update shops set name=?, adress=? where id=?;";
+		Connection connection = con.getConnection();
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(query);
+			pstmt.setString(1,shop.getName());
+			pstmt.setString(2,shop.getAdress());
+			pstmt.setInt(3,shop.getId());
+			pstmt.executeUpdate();
+			System.out.println("shops is updated");
+		} catch (SQLException e) {
+			System.out.println("bad query");
+		}
 	}
 	
 	public void deleteCars(Cars car){
-		
+		String query = "Delete from cars where id=?;";
+		Connection connection = con.getConnection();
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(query);
+			pstmt.setInt(1,car.getId());
+			pstmt.executeUpdate();
+			System.out.println("car is deleted");
+		} catch (SQLException e) {
+			System.out.println("bad query");
+		}
 	}
 	
 	public void deleteShops(Shops shop){
-		
+		String query = "Delete from shops where id=?;";
+		Connection connection = con.getConnection();
+		try {
+			PreparedStatement pstmt = connection.prepareStatement(query);
+			pstmt.setInt(1,shop.getId());
+			pstmt.executeUpdate();
+			System.out.println("shop is deleted");
+		} catch (SQLException e) {
+			System.out.println("bad query");
+		}
 	}
 	
 }
